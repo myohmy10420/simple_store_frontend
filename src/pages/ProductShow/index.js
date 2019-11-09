1dfc658e1afd40b5eb655c06fdb2fdcc9572907b
KurtHsu <myohmy10420@gmail.com>
2019-11-10 00:21:46 +0800

    template ProductShow

--- src/pages/ProductShow/index.js
+++ src/pages/ProductShow/index.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import {
   Card, CardText, CardBody, CardTitle, Button, CardLink
 } from 'reactstrap'
 
 import './index.scss'
 
-const ProductList = () => {
+const ProductShow = (props) => {
+  const product_id = props.match.params.id
+  const [haveAsyncData, setHaveAsyncData] = useState(false)
+  const [product, setProduct] = useState({})
+
+  if ( !haveAsyncData ) {
+    setHaveAsyncData(true)
+    axios({
+      url: `${process.env.REACT_APP_API}/api/v1/products/${product_id}`,
+      method: 'GET'
+    }).then(response => {
+      const { data } = response
+      setProduct(data.product)
+    }).catch(error => {
+      alert('讀取商品失敗')
+    })
+  }
   return (
     <Card className='product'>
       <CardBody>
-        <CardTitle>商品名稱</CardTitle>
+        <CardTitle>商品: {product.name}</CardTitle>
         <CardText>
-          <span>商品內容</span><br/>
-          <span>價格: 1000</span>
+          <span>{product.content}</span><br/><br/>
+          <span>價格: {product.price}</span><br/>
+          <span>剩餘數量: {product.inventory}</span>
         </CardText>
         <CardLink href="/products">
           <Button>回商品列表</Button>
@@ -25,4 +43,4 @@ const ProductList = () => {
   );
 }
 
-export default ProductList
+export default ProductShow
